7a7898af12da37814ca5feaee47530b8eb7d463b
Zhuk Aleksandr <jobbox.zh@gmail.com>
2022-08-25 13:23:57 +0300

    validated

--- src/js/script.js
+++ src/js/script.js
@@ -115,8 +115,26 @@ $(document).ready(function(){
             $('#consultation, #order').fadeOut();
             $('.overlay, #thanks').fadeIn(slow);
             $('form').trigger('reset');            
-        })
+        });
+        return false;
     });
-    return false;
+    
+    //smooth scroll
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();            
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#up']").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
 
 });
